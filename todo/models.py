5918aef37181696b7e3a87490781c79daf4ef741
Mowka7 <dadzaiosamu5@gmail.com>
2022-11-14 12:42:31 +0000

    Created crud for tags and task models

--- todo/models.py
+++ todo/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Tags(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.CharField(max_length=255)
     datetime = models.DateTimeField(auto_now_add=True)
     progress = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tags, related_name="tasks")
+
+    class Meta:
+        ordering = ["progress", "-datetime"]
